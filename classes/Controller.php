56048f4525ab17fcf49b6580d1f4f295fd989a21
Adeoti Damilola <57512119+damiultimate@users.noreply.github.com>
2020-10-18 08:23:06 +0100

    Update Controller.php

--- classes/Controller.php
+++ classes/Controller.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Support;  // https://laravel.com/docs/5.8/collections - provides the collect methods & collections class
 use LSS\Array2Xml;
-require_once('classes/Exporter.php');
+require_once('Exporter.php');
 
 
 class Controller {
